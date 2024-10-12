31a3dd48413c3cdb7df2b6ee6265e20c7905cb53
khalidmarzou <khalidmarzoug9@gmail.com>
2024-10-12 14:57:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>
+        Hello World From <span className="text-red-500 px-3"> React JS </span> Deployed
+      </h1>
+    </>
+  );
+}
+
+export default App;

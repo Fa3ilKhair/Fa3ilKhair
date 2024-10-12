4d487fb4989cf93bb0fa67d1197dd35814f55a2b
zikoo <sidqui.zakaria1@gmail.com>
2024-10-12 18:53:30 +0100

    folder structure

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,5 +1,16 @@
+import Button from "../smallComponents/Button";
+import Link from "../smallComponents/Link";
 export default function Nav(){
     return(
-        <nav>hello Nav</nav>
+        <nav className="flex  border-green-600 w-[60%] ">
+             <ul className="flex justify-evenly items-center  w-[100%]">
+                <li><Link content="Acceuil" route="/"/></li>
+                <li><Link content="Projets" route="/projets"/></li>
+                <li><Link content="Galerie" route="/gelerie"/></li>
+                <li><Link content="Contactez-nous" route="/"/></li>
+                <li><Link content="Qui sommes-nous?" route="/"/></li>
+                <li><Button content='Faire un Don' color='bg-green-600' /></li>
+            </ul>
+        </nav>
     );
 }
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-
 export default function NotFoundPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full h-full">

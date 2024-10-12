ce2371af5652605cf3d14527e54bc883af7c8434
khalidmarzou <khalidmarzoug9@gmail.com>
2024-10-12 15:52:40 +0100

    add routing react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Index from "./pages/Index";
+import Projects from "./pages/Projects";
+import Galerie from "./pages/Galerie";
+import NotFoundPage from "./pages/NotFoundPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/Fa3ilKhair",
+    element: <Index />,
+  },
+  {
+    path: "/Fa3ilKhair/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/Fa3ilKhair/galerie",
+    element: <Galerie />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);

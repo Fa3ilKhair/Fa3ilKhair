c9c88cad49c558f89de1aaf2bf0e11710c30e389
zikoo <sidqui.zakaria1@gmail.com>
2024-10-12 21:10:54 +0100

    fin part qui sommes nous

--- src/components/layout/presentationSection.jsx
+++ src/components/layout/presentationSection.jsx
@@ -3,13 +3,15 @@ export default function PresentationSection(){
     return(
         <section className=" px-20 py-20 bg-white flex justify">
             <img className="h-96 pr-24" src="/Fa3ilKhair/assets/presentationImg.png" alt="assosiation.png" />
-            <div className="flex flex-col justify-evenly ">
+            <div className="flex flex-col justify-evenly items-start ">
                 <h1 className="font-bold text-4xl">Qui sommes-nous?</h1>
-                <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nulla quos voluptatum odit commodi vel iure vero, itaque fugiat,
-                    iste consequuntur? Iusto accusamus suscipit eligendi voluptatibus laboriosam quaerat corrupti aliquam.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nulla quos voluptatum odit commodi vel iure vero, itaque fugiat,
-                    iste consequuntur? Iusto accusamus suscipit eligendi voluptatibus laboriosam quaerat corrupti aliquam</p>
-                <Button content="En savoir plus" color="bg-[#4CAF4F]"/>
+                <p className="text-xl text-justify">Chaque jour, notre pays fait un nouveau pas vers un avenir meilleur.
+                    Nous, association Mubadarat Fail Khir Rabat, affirmons que l’engagement associatif est une des clés de ce mouvement.
+                    Face aux différents défis, nous prenons depuis 2012, notre part à la construction d’un futur marocain plus solidaire.
+                    C’est l’ambition qui anime le cœur de notre association, 
+                    le sens de la responsabilité envers nos concitoyens est une résolution de ceux qui ont fait le 
+                    choix d’être volontaires au service de l’intérêt général.</p>
+                <Button content="En savoir plus" color="bg-[#4CAF4F] "/>
             </div>
         </section>
     );
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
+import Main from "../components/layout/Main";
 
 export default function Index() {
   return (
     <>
       <Header />
+      <Main/>
       <Footer />
     </>
   );

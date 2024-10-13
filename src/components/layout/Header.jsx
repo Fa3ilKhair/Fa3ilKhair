c98c07674fcd3aa9dc4093aa7ec05d52c57689ac
khalidmarzou <khalidmarzoug9@gmail.com>
2024-10-13 14:48:26 +0100

    resolve problem auto-scroll-Y with removing width=screen and replace it with width=full

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../smallComponents/Logo";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 h-[10%]  bg-white shadow-xl flex justify-center items-center py-4 px-6 w-full">
+    <header className="sticky top-0 bg-white shadow-xl flex justify-center items-center py-4 px-6 w-full">
       <div className="border-blue-800 flex justify-between w-full px-6">
         <Logo taille="h-16" />
         <Nav />

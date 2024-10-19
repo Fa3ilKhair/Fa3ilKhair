0ea2bfbc2482f39ceba7546a9c6447a598862bed
khalidmarzou <khalidmarzoug@gmail.com>
2024-10-19 20:13:51 +0100

    cart maps exemple finished

--- src/components/layout/Maps.jsx
+++ src/components/layout/Maps.jsx
@@ -11,11 +11,11 @@ const locations = [
     description:
       "À Casablanca, Fa3il Khair a organisé des ateliers éducatifs qui ont bénéficié à plus de 150 jeunes. Ces ateliers incluaient la formation aux compétences, le tutorat et des programmes de mentorat, aidant les participants à acquérir des connaissances précieuses pour leurs futures carrières. L'initiative visait à responsabiliser la jeunesse, lui permettant de contribuer positivement à ses communautés.",
     images: [
-      "https://via.placeholder.com/150/FF0000/FFFFFF?text=Casablanca+1",
-      "https://via.placeholder.com/150/FF0000/FFFFFF?text=Casablanca+2",
-      "https://via.placeholder.com/150/FF0000/FFFFFF?text=Casablanca+3",
-      "https://via.placeholder.com/150/FF0000/FFFFFF?text=Casablanca+4",
-      "https://via.placeholder.com/150/FF0000/FFFFFF?text=Casablanca+5",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
     ],
   },
   {
@@ -25,11 +25,11 @@ const locations = [
     description:
       "À Tanger, Fa3il Khair a assisté les communautés locales avec des projets environnementaux, impliquant plus de 200 bénévoles. Cette initiative a consisté à nettoyer les plages et les parcs locaux, promouvant la sensibilisation à l'environnement parmi les résidents. Le projet a non seulement embelli la région, mais a également sensibilisé les participants à la durabilité et à la conservation.",
     images: [
-      "https://via.placeholder.com/150/00FF00/FFFFFF?text=Tanger+1",
-      "https://via.placeholder.com/150/00FF00/FFFFFF?text=Tanger+2",
-      "https://via.placeholder.com/150/00FF00/FFFFFF?text=Tanger+3",
-      "https://via.placeholder.com/150/00FF00/FFFFFF?text=Tanger+4",
-      "https://via.placeholder.com/150/00FF00/FFFFFF?text=Tanger+5",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
     ],
   },
   {
@@ -39,11 +39,11 @@ const locations = [
     description:
       "À Marrakech, Fa3il Khair a soutenu des événements culturels pour les jeunes, atteignant environ 100 participants. Les activités comprenaient des ateliers d'art, des sessions de musique et des cours de danse. Cette initiative a encouragé la créativité parmi les participants et favorisé un sentiment de communauté à travers l'échange culturel.",
     images: [
-      "https://via.placeholder.com/150/0000FF/FFFFFF?text=Marrakech+1",
-      "https://via.placeholder.com/150/0000FF/FFFFFF?text=Marrakech+2",
-      "https://via.placeholder.com/150/0000FF/FFFFFF?text=Marrakech+3",
-      "https://via.placeholder.com/150/0000FF/FFFFFF?text=Marrakech+4",
-      "https://via.placeholder.com/150/0000FF/FFFFFF?text=Marrakech+5",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
     ],
   },
   {
@@ -53,11 +53,11 @@ const locations = [
     description:
       "À Rabat, Fa3il Khair a promu la sensibilisation aux services de santé, touchant plus de 300 personnes. La campagne a inclus des dépistages de santé, des ateliers sur la nutrition et la sensibilisation à la santé mentale. En engageant la communauté, l'initiative a aidé à améliorer la littératie en santé et l'accès aux services.",
     images: [
-      "https://via.placeholder.com/150/FFFF00/FFFFFF?text=Rabat+1",
-      "https://via.placeholder.com/150/FFFF00/FFFFFF?text=Rabat+2",
-      "https://via.placeholder.com/150/FFFF00/FFFFFF?text=Rabat+3",
-      "https://via.placeholder.com/150/FFFF00/FFFFFF?text=Rabat+4",
-      "https://via.placeholder.com/150/FFFF00/FFFFFF?text=Rabat+5",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
     ],
   },
   {
@@ -67,14 +67,36 @@ const locations = [
     description:
       "À Fès, Fa3il Khair a fourni une formation aux artisans, bénéficiant à environ 50 artisans. Ce programme s'est concentré sur l'amélioration des compétences traditionnelles et l'introduction de nouvelles techniques, permettant aux artisans d'améliorer leur métier et de mieux commercialiser leurs produits. L'initiative visait à préserver le patrimoine culturel tout en soutenant les économies locales.",
     images: [
-      "https://via.placeholder.com/150/FF00FF/FFFFFF?text=Fès+1",
-      "https://via.placeholder.com/150/FF00FF/FFFFFF?text=Fès+2",
-      "https://via.placeholder.com/150/FF00FF/FFFFFF?text=Fès+3",
-      "https://via.placeholder.com/150/FF00FF/FFFFFF?text=Fès+4",
-      "https://via.placeholder.com/150/FF00FF/FFFFFF?text=Fès+5",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
+      "/Fa3ilKhair/assets/outils/cat.jpg",
     ],
   },
 ];
+
+const customIcon = new L.Icon({
+  iconUrl: "/Fa3ilKhair/assets/outils/marker-icon.png", // Path to your local icon image
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // The point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // The point from which the popup should open relative to the iconAnchor
+});
 
 export default function Maps({ onLocationSelect }) {
   const handleMarkerClick = useCallback(
@@ -91,6 +113,7 @@ export default function Maps({ onLocationSelect }) {
         <Marker
           key={index}
           position={[loc.lat, loc.lng]}
+          icon={customIcon} // Use the custom icon
           eventHandlers={{
             click: () => handleMarkerClick(loc),
           }}
--- src/components/layout/MapsSection.jsx
+++ src/components/layout/MapsSection.jsx
@@ -8,8 +8,21 @@ export default function MapsSection() {
     images: [],
   });
 
+  const [isModalOpen, setIsModalOpen] = useState(false); // State for modal visibility
+  const [selectedImage, setSelectedImage] = useState(""); // State for selected image
+
   const handleLocationSelect = (location) => {
     setSelectedLocation(location);
+  };
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage("");
   };
 
   return (
@@ -20,13 +33,26 @@ export default function MapsSection() {
         <article className="w-1/3 overflow-auto h-auto phone:px-5 phone:w-full phone:mt-10">
           <h2 className="text-xl font-bold">{selectedLocation.title}</h2>
           <p>{selectedLocation.description}</p>
-          <div className="grid grid-cols-2 gap-2">
+          <div className="mt-10 flex w-full justify-center flex-wrap gap-5 gap-y-5 h-96">
             {selectedLocation.images.map((image, index) => (
-              <img key={index} src={image} alt={selectedLocation.title} className="h-24" />
+              <img
+                key={index}
+                src={image}
+                alt={selectedLocation.title}
+                className="w-1/4 cursor-pointer" // Add cursor pointer for clickable images
+                onClick={() => openModal(image)} // Open modal on click
+              />
             ))}
           </div>
         </article>
       </section>
+
+      {/* Modal for displaying the selected image */}
+      {isModalOpen && (
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50" onClick={closeModal}>
+          <img src={selectedImage} alt="Selected" className="max-w-full max-h-full p-5" />
+        </div>
+      )}
     </>
   );
 }

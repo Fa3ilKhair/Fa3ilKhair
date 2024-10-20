28e27cb056ed7c67d34868dbaec3dbc669b99718
zikoo <sidqui.zakaria1@gmail.com>
2024-10-20 15:06:54 +0100

    final part  Dernier projet

--- src/components/layout/DernierProjets.jsx
+++ src/components/layout/DernierProjets.jsx
@@ -9,9 +9,7 @@ export default function DernierProjets(){
         Nos derniers projets
       </h1>
       <p className="mt-5 w-2/4 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit,
-        diam vitae consequat porta, quam quam quam quam ante euismod tellus, vitae consequat metus ante vel nibh.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean hendrerit,
-        diam vitae consequat porta, quam quam quam quam ante euismod tellus, vitae consequat metus ante vel nibh.</p>
+        diam vitae consequat porta, quam quam quam quam ante euismod tellus, vitae consequat metus ante vel nibh</p>
       <div className="flex justify-evenly mt-24 w-full phone:flex-col phone:items-center phone:gap-6">
         <DernierProjectCard text={"Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise"} img={"/Fa3ilKhair/assets/media/dernierProjets.jpg"}/>
         <DernierProjectCard text={"Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise"}  img={"/Fa3ilKhair/assets/media/dernierProjets2.jpg"}/>
